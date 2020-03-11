64c197a2f0567b6204ecb279e6d18ae051ee10de
Ipalnemoani <56195776+Ipalnemoani@users.noreply.github.com>
2020-03-11 19:07:47 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,18 @@
+import os
+
+APISERVER = os.environ['APISERVER']
+PORT = eval(os.environ['PORT'])
+HTTP_LOGIN = os.environ['HTTP_LOGIN']
+HTTP_PASSWORD = os.environ['HTTP_PASSWORD']
+MAININDEX = os.environ['MAININDEX']
+MAINCOLLECTION = os.environ['MAINCOLLECTION']
+SOURCE = os.environ['SOURCE']
+TIMEZONE = os.environ['TIMEZONE']
+COUNTRY = os.environ['COUNTRY']
+REGION = os.environ['REGION']
+OUT_LOG = os.environ['OUT_LOG']
+CSV_PATH = os.environ['CSV_PATH']
+MAX_POSTS = os.environ['MAX_POSTS']
+LOGIN = os.environ['LOGIN']
+PASSWORD = os.environ['PASSWORD']
+SLEEPING_TIME = eval(os.environ['SLEEPING_TIME'])
